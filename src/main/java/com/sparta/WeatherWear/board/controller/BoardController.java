9ba11d5eb8458fe7b196fa5e0dede81a717c15d0
Ha Junyoung <jun19973@naver.com>
2024-07-30 20:07:35 +0900

    Board Restful 수정

--- src/main/java/com/sparta/WeatherWear/board/controller/BoardController.java
+++ src/main/java/com/sparta/WeatherWear/board/controller/BoardController.java
@@ -20,37 +20,37 @@ public class BoardController {
     private final BoardService boardService;
 
     /* 게시물 작성 */
-    @PostMapping("/create")
+    @PostMapping("/")
     public ResponseEntity<ApiResponse<BoardCreateResponseDto>> createBoard(@RequestBody BoardCreateRequestDto requestDto, @Valid @RequestParam("images") List<MultipartFile> images) {
         return boardService.createBoard(requestDto, images);
     }
 
     /* 게시물 id로 조회 */
-    @GetMapping("/find/{board_id}")
+    @GetMapping("/board-id/{board_id}")
     public ResponseEntity<ApiResponse<BoardCreateResponseDto>> findBoardById(@PathVariable Long board_id) {
         return boardService.findBoardById(board_id);
     }
 
     /* 게시물 user_id 전체 목록 조회 (페이징) */
-    @GetMapping("/find/{user_id}")
+    @GetMapping("/user-id/{user_id}")
     public ResponseEntity<ApiResponse<List<BoardCreateResponseDto>>> findBoardByUserId(@PathVariable Long user_id) {
         return boardService.findBoardByUserId(user_id);
     }
 
     /* 게시물 전체 목록 조회 (페이징) & 아이디에 해당하는 값 있으면 수정 기능 추가하기 */
-    @GetMapping("/find-all")
+    @GetMapping("/all")
     public ResponseEntity<ApiResponse<List<BoardCreateResponseDto>>> findBoardAll() {
         return boardService.findBoardAll();
     }
 
     /* 게시물 수정 */
-    @PutMapping("/update")
+    @PutMapping("/")
     public ResponseEntity<ApiResponse<BoardCreateResponseDto>> updateBoard(@RequestBody BoardUpdateRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam("images") List<MultipartFile> images) {
         return boardService.updateBoard(requestDto, userDetails, images);
     }
 
     /* 게시물 삭제 (게시물을 작성한 유저가 맞는지) */
-    @DeleteMapping("/remove/{board_id}")
+    @DeleteMapping("/{board_id}")
     public ResponseEntity<String> removeBoard(@PathVariable Long board_id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return boardService.removeBoard(board_id, userDetails);
     }
