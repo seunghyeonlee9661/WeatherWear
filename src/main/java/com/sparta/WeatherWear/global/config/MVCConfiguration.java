b461ad7db45822a2eeb809c0c61be807c32e3ee1
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-02 18:09:59 +0900

    Update MVCConfiguration.java

--- src/main/java/com/sparta/WeatherWear/global/config/MVCConfiguration.java
+++ src/main/java/com/sparta/WeatherWear/global/config/MVCConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
