d36699f8dffb0c7c6b5e62ea75c19cb958fedcd5
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-09 20:49:56 +0900

    사용자 게시물 추가

--- src/main/java/com/sparta/WeatherWear/board/dto/TagResponseDTO.java
+++ src/main/java/com/sparta/WeatherWear/board/dto/TagResponseDTO.java
@@ -3,7 +3,9 @@ package com.sparta.WeatherWear.board.dto;
 import com.sparta.WeatherWear.board.entity.BoardTag;
 import com.sparta.WeatherWear.clothes.enums.ClothesColor;
 import com.sparta.WeatherWear.clothes.enums.ClothesType;
+import lombok.Getter;
 
+@Getter
 public class TagResponseDTO {
     private ClothesColor color;
     private ClothesType type;
