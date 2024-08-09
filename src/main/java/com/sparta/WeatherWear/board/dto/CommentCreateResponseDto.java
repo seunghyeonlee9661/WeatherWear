a1867e95fea13f0e66a98593a83d024f15f11643
Ha Junyoung <jun19973@naver.com>
2024-08-09 23:29:26 +0900

    userSimpleDto ===> user 로 수정 user 안에 있는 userId => id

--- src/main/java/com/sparta/WeatherWear/board/dto/CommentCreateResponseDto.java
+++ src/main/java/com/sparta/WeatherWear/board/dto/CommentCreateResponseDto.java
@@ -1,7 +1,6 @@
 package com.sparta.WeatherWear.board.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sparta.WeatherWear.board.entity.Board;
 import com.sparta.WeatherWear.board.entity.Comment;
 import com.sparta.WeatherWear.user.dto.UserSimpleDto;
 import lombok.Getter;
@@ -9,7 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,7 +15,7 @@ import java.util.List;
 public class CommentCreateResponseDto {
     private long id;
     // 사용자 정보
-    private UserSimpleDto userSimpleDto = new UserSimpleDto();
+    private UserSimpleDto user = new UserSimpleDto();
 
     private Long BoardId;
     private String contents;
@@ -30,9 +28,9 @@ public class CommentCreateResponseDto {
     public CommentCreateResponseDto(Comment comment) {
         this.id = comment.getId();
         //
-        this.userSimpleDto.setUserId(comment.getUser().getId());
-        this.userSimpleDto.setNickname(comment.getUser().getNickname());
-        this.userSimpleDto.setImage(comment.getUser().getImage());
+        this.user.setId(comment.getUser().getId());
+        this.user.setNickname(comment.getUser().getNickname());
+        this.user.setImage(comment.getUser().getImage());
         //
         this.BoardId = comment.getBoard().getId();
         this.contents = comment.getContents();
--- src/main/java/com/sparta/WeatherWear/user/dto/UserSimpleDto.java
+++ src/main/java/com/sparta/WeatherWear/user/dto/UserSimpleDto.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 public class UserSimpleDto {
 
-    private Long userId;
+    private Long id;
     private String nickname;
     private String image;
 
