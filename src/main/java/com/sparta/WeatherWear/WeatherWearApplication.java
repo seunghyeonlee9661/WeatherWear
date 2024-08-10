a674743504f2f617140582392b6fdaf4ab438b92
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-10 09:39:19 +0900

    시간 처리 확인

--- src/main/java/com/sparta/WeatherWear/WeatherWearApplication.java
+++ src/main/java/com/sparta/WeatherWear/WeatherWearApplication.java
@@ -1,6 +1,7 @@
 package com.sparta.WeatherWear;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +9,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan(basePackages = "com.sparta.WeatherWear") // 필요한 패키지 경로 지정
 public class WeatherWearApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherWearApplication.class, args);
 	}
