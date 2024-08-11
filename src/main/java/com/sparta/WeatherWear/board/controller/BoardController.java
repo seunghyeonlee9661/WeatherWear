9d99ecaac11d101fb1e20513a8b5650b9ca3a5a0
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-11 20:19:03 +0900

    게시물 좋아요 수 잘 나오도록 수정

--- src/main/java/com/sparta/WeatherWear/board/controller/BoardController.java
+++ src/main/java/com/sparta/WeatherWear/board/controller/BoardController.java
@@ -72,13 +72,6 @@ public class BoardController {
     @PutMapping("")
     public ResponseEntity<?> updateBoard( @RequestPart("data") String data, // data 스트링
                                           @RequestPart(value = "file", required = false) MultipartFile image, // 파일 정보
-//                                          @RequestPart("boardId") @NotNull(message = "boardId가 없습니다.") Long boardId,
-//                                          @RequestPart("address") @NotBlank(message = "주소값이 없습니다.") String address,
-//                                          @RequestPart("addressId") @NotNull(message = "행정동 코드값이 없습니다.") Long addressId,
-//                                          @RequestPart("title") @NotBlank(message = "제목이 없습니다.") String title,
-//                                          @RequestPart("contents") @NotBlank(message = "내용이 없습니다.") String contents,
-//                                          @RequestPart("isPrivate") boolean isPrivate,
-//                                          @RequestPart("tags") List<ClothesRequestDTO> tags,
                                           @AuthenticationPrincipal UserDetailsImpl userDetails  ) throws IOException {
         BoardUpdateRequestDto requestDto = new ObjectMapper().readValue(data, BoardUpdateRequestDto.class); // json 형식을 DTO로 파싱합니다.
         return boardService.updateBoard(requestDto, userDetails, image);
