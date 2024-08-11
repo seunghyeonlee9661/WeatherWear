4833fb7447fa9c074c84ff1aaa1737a17cca4e74
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-11 19:43:48 +0900

    추천 아이템 비로그인 사용자 허용 - 트렌드 전체 조회

--- src/main/java/com/sparta/WeatherWear/user/service/RecommendService.java
+++ src/main/java/com/sparta/WeatherWear/user/service/RecommendService.java
@@ -69,10 +69,11 @@ public class RecommendService {
         // 날씨값 찾기
         Weather weather = weatherRepository.getWeatherById(id).orElseThrow(()-> new IllegalArgumentException("날씨 ID가 올바르지 않습니다."));
 
-        User user = null;
+        System.out.println("초기 유저 null");
         // 로그인한 사용자일 경우 각 항목에 대해 모두 추천 리스트를 받는다.
         if(userDetails != null){
-            user = userDetails.getUser();
+            System.out.println("userDetails != null -> 로그인 사용자");
+            User user = userDetails.getUser();
             /* 1. 날씨 기반 옷차림 추천 */
             recommendResponseDTOS.add(getClothesByWeather(user, weather));
             /* 2. 내 옷차림 추천 : 내 게시물 / 현재 장소와 시간의 날씨와 유사한  */
@@ -82,7 +83,8 @@ public class RecommendService {
             /* 4. 네이버 아이템 추천 */
             recommendResponseDTOS.add(getNaverProductsByWeather(user, weather));
         }else{
-            recommendResponseDTOS.add(getBoardsByTrends(user, weather));
+            System.out.println("userDetails == null -> 비로그인 사용자");
+            recommendResponseDTOS.add(getBoardsByTrends(null, weather));
         }
         return recommendResponseDTOS;
     }
