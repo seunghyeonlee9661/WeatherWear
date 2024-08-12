5db00641d69bb8761b99d6c7280d429815294e9a
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-12 14:48:44 +0900

    my ootd에 대해 검색 결과 필터 변환

--- src/main/java/com/sparta/WeatherWear/board/repository/BoardRepository.java
+++ src/main/java/com/sparta/WeatherWear/board/repository/BoardRepository.java
@@ -57,7 +57,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
             "WHERE b.user.id = :userId " +
             "AND ( " +
             "  (:pty IS NULL AND :sky IS NULL) " +
-            "  (:pty = 1 AND (w.PTY = 1 OR w.PTY = 2 OR w.PTY = 4)) " +
+            "  OR (:pty = 1 AND (w.PTY = 1 OR w.PTY = 2 OR w.PTY = 4)) " +
             "  OR (:pty = 3 AND w.PTY = 3) " +
             "  OR (:pty IS NULL AND (:sky IS NOT NULL AND w.SKY = :sky AND w.PTY = 0)) " +
             ") " +
