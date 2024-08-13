0aea9afa9a1cac89b15475f42d10295c20264ff7
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-14 00:35:47 +0900

    접근자 로그 확인

--- src/main/java/com/sparta/WeatherWear/global/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/WeatherWear/global/filter/JwtAuthorizationFilter.java
@@ -36,8 +36,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     /* 사용자의 로그인 상태를 토큰을 통해 검증합니다. 또한 RefreshToken을 활용해 토큰 탈취에 대비합니다. */
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
-        String requestURI = req.getRequestURI();
-
         String accessToken  = jwtUtil.getTokenFromRequest(req, JwtUtil.AUTHORIZATION_HEADER); // 액세스 토큰과 리프레시 토큰을 쿠키에서 가져옴
         if (accessToken != null) { // accessToken 확인
             String accessTokenValue = jwtUtil.substringToken(accessToken); // accessToken 검증
@@ -51,6 +49,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                     UserDetails userDetails = userDetailsService.loadUserByUsername(jwtUtil.getUserInfoFromToken(jwtUtil.substringToken(newAccessToken)).getSubject()); // 새 토큰에서 사용자 정보를 추출
                     jwtUtil.addTokenToCookie(newAccessToken, res);// 새로운 액세스 토큰을 쿠키에 추가
                     setAuthentication(userDetails, req);// 사용자 인증 설정
+                    log.info("User = {}",userDetails.getUsername() );
                 }
             }
         }
