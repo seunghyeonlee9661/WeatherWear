effb14ac16e6ad0d9b38aaad3d2464eacd3c55a5
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-14 19:20:35 +0900

    실행 코드 수정

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 애플리케이션 시작 스크립트
 
-cd /home/ubuntu/deployments/Weatherwear
-nohup ./gradlew bootrun > /home/ubuntu/deployments/Weatherwear/logs/application.log 2>&1 &
+cd /home/ubuntu/spring/WeatherWear
+nohup ./gradlew bootrun > /home/ubuntu/spring/WeatherWear/logs/application.log 2>&1 &
