b04bed632fd130eff43cb71054bf398583ce5df9
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-15 16:52:11 +0900

    배포 파일 변경

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-# 애플리케이션 시작 스크립트
 
-cd /home/ubuntu/spring/WeatherWear
-nohup ./gradlew bootrun > /home/ubuntu/spring/WeatherWear/logs/application.log 2>&1 &
+# 로그 파일 정의
+LOGFILE="/home/ubuntu/spring/WeatherWear/logs/startup.log"
+
+# 현재 시간 출력
+echo "Starting application at $(date)" >> $LOGFILE
+
+# 애플리케이션 디렉토리로 이동
+echo "Changing directory to /home/ubuntu/spring/WeatherWear" >> $LOGFILE
+cd /home/ubuntu/spring/WeatherWear >> $LOGFILE 2>&1
+
+# 애플리케이션 실행
+echo "Starting application with gradlew bootrun" >> $LOGFILE
+nohup ./gradlew bootrun >> /home/ubuntu/spring/WeatherWear/logs/application.log 2>&1 &
+echo "Application started at $(date)" >> $LOGFILE
